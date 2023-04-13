2276f68d10519a834b831d4f12956b105d476181
lsulevi <qqqqban@gmail.com>
2023-04-13 15:14:59 +0800

    初始commit

---
+++ SpringBoot-voteSystem/src/main/java/com/ivan/vote/SpringBootVoteSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.vote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVoteSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVoteSystemApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-voteSystem/src/test/java/com/ivan/vote/SpringBootVoteSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan.vote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVoteSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
